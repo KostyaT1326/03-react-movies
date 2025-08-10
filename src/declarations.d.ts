ed621709412ec2cfd087c4b3aa5f1e67c786a163
Kostya Tkachenko <tkostyantin@gmail.com>
2025-08-10 14:05:47 +0300

    hw-2-ts

---
+++ src/declarations.d.ts
@@ -0,0 +1,9 @@
+declare module "modern-normalize";
+
+declare module "*.css";
+
+// declare module "*.module.css" {
+//   const classes: { [key: string]: string };
+//   export default classes;
+// }
+
